89ae405a6abaf56a2e3f6cf413362a868b9e3a5f
Hale123-cloud <leha24052004@gmail.com>
2025-07-25 12:22:42 +0700

    Add files via upload

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProductList } from './product-list/product-list';
+import { Child } from "./child/child";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ProductList, Child],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected title = 'angular-su25';
+}
